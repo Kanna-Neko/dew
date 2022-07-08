b99b5160a0631af1ab16095dc5858fc2197659d7
jaxleof <moonandxinxin@qq.com>
2022-07-08 20:12:11 +0800

    refactor: Open

--- cmd/open.go
+++ cmd/open.go
@@ -21,15 +21,7 @@ var Open = &cobra.Command{
 	Use:   "open",
 	Short: "a shortcut of opening codeforces website",
 	Run: func(cmd *cobra.Command, args []string) {
-		if runtime.GOOS == "darwin" {
-			var comm = exec.Command("open", "https://codeforces.com")
-			err := comm.Run()
-			if err != nil {
-				log.Fatal(err)
-			}
-		} else {
-			log.Fatal(runtime.GOOS, "have not supported")
-		}
+		OpenWebsite("https://codeforces.com")
 	},
 }
 
@@ -37,15 +29,7 @@ var OpenGyms = &cobra.Command{
 	Use:   "gym",
 	Short: "a shortcut of opening codeforces gyms",
 	Run: func(cmd *cobra.Command, args []string) {
-		if runtime.GOOS == "darwin" {
-			var comm = exec.Command("open", "https://codeforces.com/gyms")
-			err := comm.Run()
-			if err != nil {
-				log.Fatal(err)
-			}
-		} else {
-			log.Fatal(runtime.GOOS, "have not supported")
-		}
+		OpenWebsite("https://codeforces.com/gyms")
 	},
 }
 
@@ -53,15 +37,7 @@ var OpenMashup = &cobra.Command{
 	Use:   "mashup",
 	Short: "a shortcut of opening codeforces mashup",
 	Run: func(cmd *cobra.Command, args []string) {
-		if runtime.GOOS == "darwin" {
-			var comm = exec.Command("open", "https://codeforces.com/mashups")
-			err := comm.Run()
-			if err != nil {
-				log.Fatal(err)
-			}
-		} else {
-			log.Fatal(runtime.GOOS, "have not supported")
-		}
+		OpenWebsite("https://codeforces.com/mashups")
 	},
 }
 
@@ -69,15 +45,7 @@ var OpenStatus = &cobra.Command{
 	Use:   "status",
 	Short: "a shortcut of opening codeforces status",
 	Run: func(cmd *cobra.Command, args []string) {
-		if runtime.GOOS == "darwin" {
-			var comm = exec.Command("open", "https://codeforces.com/problemset/status?my=on")
-			err := comm.Run()
-			if err != nil {
-				log.Fatal(err)
-			}
-		} else {
-			log.Fatal(runtime.GOOS, "have not supported")
-		}
+		OpenWebsite("https://codeforces.com/problemset/status?my=on")
 	},
 }
 
@@ -87,6 +55,18 @@ var OpenRandom = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		OpenRandomFunc()
 	},
+}
+
+func OpenWebsite(path string) {
+	if runtime.GOOS == "darwin" {
+		var comm = exec.Command("open", path)
+		err := comm.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		log.Fatal(runtime.GOOS, "have not supported")
+	}
 }
 
 func OpenRandomFunc() {
