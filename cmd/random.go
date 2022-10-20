d53807ff56b77510037bb5b20368695a8608e086
jaxleof <moonandxinxin@qq.com>
2022-10-21 02:18:05 +0800

    feat: random range [min,max]

--- cmd/random.go
+++ cmd/random.go
@@ -1,6 +1,12 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"log"
+	"strconv"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
 
 func init() {
 	rootCmd.AddCommand(random)
@@ -9,7 +15,49 @@ func init() {
 var random = &cobra.Command{
 	Use:   "random",
 	Short: "alias to cf generate random",
+	Args:  cobra.MaximumNArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
-		Random()
+		if len(args) == 1 {
+			rating, err := strconv.Atoi(args[0])
+			if err != nil {
+				log.Fatal(err)
+			}
+			randomMinMax(rating,rating)
+		} else if len(args) == 2 {
+			minRating, err := strconv.Atoi(args[0])
+			if err != nil {
+				log.Fatal(err)
+			}
+			maxRating, err := strconv.Atoi(args[1])
+			if err != nil {
+				log.Fatal(err)
+			}
+			randomMinMax(minRating,maxRating)
+		} else {
+			Random()
+		}
 	},
+}
+
+func randomMinMax(min, max int) {
+	if min % 100 != 0 {
+		min = min - min % 100 + 100;
+	}
+	max -= max%100;
+	viper.SetConfigFile("./codeforces/config.yaml")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var pro []string
+	for i := min; i <= max; i+=100 {
+		pro = append(pro, strconv.Itoa(i))
+	}
+	var thisOne = PickOneProblem(pro)
+	viper.Set("random", strconv.Itoa(thisOne.ContestId)+thisOne.Index)
+	err = viper.WriteConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	OpenRandomFunc(thisOne)
 }
