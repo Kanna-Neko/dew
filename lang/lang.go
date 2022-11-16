c646f9b23f00dfc3a8659338f4e8a68c17fb8d18
jaxleof <moonandxinxin@qq.com>
2022-11-16 16:27:11 +0800

    feat: support go/c/c++17 language

--- lang/lang.go
+++ lang/lang.go
@@ -14,17 +14,25 @@ type Lang struct {
 	Name             string
 }
 
-var LangSlice = []string{"c++", "c++17"}
+var LangSlice = []string{"c", "c++", "c++17", "go"}
 
 //language dictionary
 var LangDic map[string]Lang = map[string]Lang{
+	"c": {
+		IsComplieLang:    true,
+		CompileCode:      func(codefile string) *exec.Cmd { return exec.Command("gcc", codefile, "-o", "cat") },
+		RunCode:          func(codefile string) *exec.Cmd { return exec.Command("./cat") },
+		ProgramTypeId:    "43",
+		OriginalCodefile: "dew.c",
+		Name:             "GNU GCC C11 5.1.0",
+	},
 	"c++": {
 		IsComplieLang:    true,
 		CompileCode:      func(codefile string) *exec.Cmd { return exec.Command("g++", codefile, "-o", "cat") },
 		RunCode:          func(codefile string) *exec.Cmd { return exec.Command("./cat") },
 		ProgramTypeId:    "61",
 		OriginalCodefile: "dew.cpp",
-		Name:             "GNU C++17 (64)",
+		Name:             "GNU G++17 9.2.0 (64 bit, msys 2)",
 	},
 	"c++17": {
 		IsComplieLang:    true,
@@ -33,5 +41,13 @@ var LangDic map[string]Lang = map[string]Lang{
 		ProgramTypeId:    "54",
 		OriginalCodefile: "dew.cpp",
 		Name:             "GNU G++17 7.3.0",
+	},
+	"go": {
+		IsComplieLang:    true,
+		CompileCode:      func(codefile string) *exec.Cmd { return exec.Command("go", "build", "-o", "cat", codefile) },
+		RunCode:          func(codefile string) *exec.Cmd { return exec.Command("./cat") },
+		ProgramTypeId:    "32",
+		OriginalCodefile: "dew.go",
+		Name:             "Go 1.19",
 	},
 }
