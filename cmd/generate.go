e5ba7b69fcd459597975356fb664f1ba673e08df
jaxleof <moonandxinxin@qq.com>
2022-11-18 16:57:22 +0800

    feat: dew generate custom

--- cmd/generate.go
+++ cmd/generate.go
@@ -2,15 +2,21 @@ package cmd
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"math/rand"
+	"os"
 	"strconv"
 	"time"
 
 	"github.com/jaxleof/dew/link"
 
 	"github.com/briandowns/spinner"
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/jaxleof/uispinner"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -26,6 +32,7 @@ func init() {
 	NewCmd.AddCommand(div2)
 	NewCmd.AddCommand(div3)
 	NewCmd.AddCommand(randomOne)
+	NewCmd.AddCommand(customCmd)
 }
 
 const (
@@ -44,28 +51,37 @@ var div3 = &cobra.Command{
 	Use:   "div3",
 	Short: "create a contest, whose difficulty like div3",
 	Run: func(cmd *cobra.Command, args []string) {
-		newContest(div3Diffculty)
+		newContest(div3Diffculty, title, duration)
 	},
 }
 var div2 = &cobra.Command{
 	Use:   "div2",
 	Short: "create a contest, whose difficulty like div2",
 	Run: func(cmd *cobra.Command, args []string) {
-		newContest(div2Diffculty)
+		newContest(div2Diffculty, title, duration)
 	},
 }
 var div1 = &cobra.Command{
 	Use:   "div1",
 	Short: "create a contest, whose difficulty like div1",
 	Run: func(cmd *cobra.Command, args []string) {
-		newContest(div1Diffculty)
+		newContest(div1Diffculty, title, duration)
 	},
 }
 
-func newContest(diffculty [][]string) {
+func newContest(diffculty [][]string, contestTitle string, contestDuration string) {
+	if contestTitle == "" {
+		contestTitle = title
+	}
+	if contestDuration == "" {
+		contestDuration = "120"
+	}
+	if len(diffculty) == 0 {
+		diffculty = div2Diffculty
+	}
 	link.Login()
 	pro := PickSomeProblem(diffculty)
-	link.CreateContest(title, duration, pro)
+	link.CreateContest(contestTitle, contestDuration, pro)
 	OpenWebsite(codeforcesDomain + "/mashups")
 }
 
@@ -167,4 +183,143 @@ func Deduplication(data []problemInfo, s map[string]bool) []problemInfo {
 		}
 	}
 	return res
+}
+
+var customCmd = &cobra.Command{
+	Use:   "custom",
+	Short: "custom a virtual contest",
+	Run: func(cmd *cobra.Command, args []string) {
+		test()
+	},
+}
+
+type ContestInfo struct {
+	Duration     string     `json:"duration"`
+	ContestTitle string     `json:"contestTitle"`
+	Name         string     `json:"name"`
+	Difficult    [][]string `json:"difficult"`
+}
+type ContestInfos struct {
+	Templates []ContestInfo `json:"templates"`
+}
+
+const listHeight = 14
+
+var (
+	titleStyle        = lipgloss.NewStyle().MarginLeft(2)
+	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
+	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
+	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
+	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
+	quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
+)
+
+type item ContestInfo
+
+func (i item) FilterValue() string { return "" }
+
+type itemDelegate struct{}
+
+func (d itemDelegate) Height() int                               { return 1 }
+func (d itemDelegate) Spacing() int                              { return 0 }
+func (d itemDelegate) Update(msg tea.Msg, m *list.Model) tea.Cmd { return nil }
+func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	i, ok := listItem.(item)
+	if !ok {
+		return
+	}
+
+	str := fmt.Sprintf("%d. %s", index+1, i.Name)
+
+	fn := itemStyle.Render
+	if index == m.Index() {
+		fn = func(s string) string {
+			return selectedItemStyle.Render("> " + s)
+		}
+	}
+
+	fmt.Fprint(w, fn(str))
+}
+
+type model struct {
+	list     list.Model
+	choice   ContestInfo
+	quitting bool
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+var temp ContestInfos
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.list.SetWidth(msg.Width)
+		return m, nil
+
+	case tea.KeyMsg:
+		switch keypress := msg.String(); keypress {
+		case "ctrl+c", "q":
+			m.quitting = true
+			return m, tea.Quit
+
+		case "enter":
+			i, ok := m.list.SelectedItem().(item)
+			if ok {
+				m.choice = ContestInfo(i)
+			}
+			return m, tea.Quit
+		}
+	}
+
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	if m.choice.Name != "" {
+		return quitTextStyle.Render(fmt.Sprintf("%s? Sounds good to you.", m.choice.Name))
+	}
+	if m.quitting {
+		return quitTextStyle.Render("No want? That’s cool.")
+	}
+	return "\n" + m.list.View()
+}
+
+func test() {
+	data, err := ioutil.ReadFile("./codeforces/contestTemplate.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(data, &temp)
+	if err != nil {
+		log.Fatal(err)
+	}
+	items := []list.Item{}
+	for _, v := range temp.Templates {
+		items = append(items, item(v))
+	}
+
+	const defaultWidth = 20
+
+	l := list.New(items, itemDelegate{}, defaultWidth, listHeight)
+	l.Title = "choose one contest template"
+	l.SetShowStatusBar(false)
+	l.SetFilteringEnabled(false)
+	l.Styles.Title = titleStyle
+	l.Styles.PaginationStyle = paginationStyle
+	l.Styles.HelpStyle = helpStyle
+
+	m := model{list: l}
+	mod, err := tea.NewProgram(m).Run()
+	if err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
+	if m, ok := mod.(model); ok && m.choice.Name != "" {
+		newContest(m.choice.Difficult, m.choice.ContestTitle, m.choice.Duration)
+	}
 }
--- go.mod
+++ go.mod
@@ -12,20 +12,36 @@ require (
 
 require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/aymanbagabas/go-osc52 v1.0.3 // indirect
+	github.com/charmbracelet/bubbles v0.14.0 // indirect
+	github.com/charmbracelet/bubbletea v0.23.1 // indirect
+	github.com/charmbracelet/lipgloss v0.6.0 // indirect
+	github.com/containerd/console v1.0.3 // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.13.0 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sahilm/fuzzy v0.1.0 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.3.0 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
@@ -35,7 +51,7 @@ require (
 require (
 	github.com/briandowns/spinner v1.18.1
 	github.com/gosuri/uilive v0.0.4 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
 	golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2 // indirect
-	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
 )
